20ebc4bfed95aad0807003473d8c0eb5658ad29e
Bobie Fordjour McCamble Kofi <fordjourbobiem@gmail.com>
2024-06-25 18:15:26 +0000

    Add new worker form sheet component

--- src/routes/dashboard/settings/members/+page.svelte
+++ src/routes/dashboard/settings/members/+page.svelte
@@ -1,6 +1,9 @@
 <script lang="ts">
     import { Button } from "@/components/button/index";
     import * as Card from "@/components/card/index";
+    import * as Sheet from "@/components/sheet/index";
+    import { Input } from "@/components/input/index";
+    import { Label } from "@/components/label/index";
 </script>
 
 <div>
@@ -14,7 +17,51 @@
                 </Card.Description>
             </Card.Header>
             <Card.Footer>
-                <Button>Create Member Account</Button>
+                <Sheet.Root>
+                    <Sheet.Trigger asChild let:builder>
+                        <Button builders={[builder]}>
+                            Create Member Account
+                        </Button>
+                    </Sheet.Trigger>
+                    <Sheet.Content side="right">
+                        <Sheet.Header>
+                            <Sheet.Title>Edit Profile</Sheet.Title>
+                            <Sheet.Description>
+                                Make changes to your profile here. Click save
+                                when you're done.
+                            </Sheet.Description>
+                        </Sheet.Header>
+                        <div class="grid gap-4 py-4">
+                            <div class="grid grid-cols-4 items-center gap-4">
+                                <Label for="name" class="text-right">
+                                    Name
+                                </Label>
+                                <Input
+                                    id="name"
+                                    value="Pedro Duarte"
+                                    class="col-span-3"
+                                />
+                            </div>
+                            <div class="grid grid-cols-4 items-center gap-4">
+                                <Label for="username" class="text-right">
+                                    Username
+                                </Label>
+                                <Input
+                                    id="username"
+                                    value="@peduarte"
+                                    class="col-span-3"
+                                />
+                            </div>
+                        </div>
+                        <Sheet.Footer>
+                            <Sheet.Close asChild let:builder>
+                                <Button builders={[builder]} type="submit">
+                                    Save Changes
+                                </Button>
+                            </Sheet.Close>
+                        </Sheet.Footer>
+                    </Sheet.Content>
+                </Sheet.Root>
             </Card.Footer>
         </Card.Root>
     </div>
